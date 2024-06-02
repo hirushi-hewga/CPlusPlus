69eebeb380a9eb20a5dfc321729bd6070566fddb
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:52:00 +0300

    Lesson (Array)

---
+++ 2024-03-08 (Array)/Lesson.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using  namespace std;
+int main() {
+    /*int train[3];
+    // cout << train[0] << endl;
+    // cout << train[1] << endl;
+    // cout << train[2] << endl;
+    train[0] = 3;
+    train[1] = 1;
+    train[2] = 4;
+    cout << train[0] << endl;
+    cout << train[1] << endl;
+    cout << train[2] << endl;
+    // cout << train[3] << endl;*/
+
+
+
+    /*int arr1[10];
+
+    const int size = 10;
+    int arr2[size];
+
+    int arr3[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout << "Address: " << arr3 << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr3[i] << " ";
+    }
+    cout << endl;
+
+    int arr4[] = {11, 12, 13, 14};
+    for (int i = 0; i < 4; i++)
+    {
+        cout << arr4[i] << " ";
+    }
+    cout << endl;
+
+    int arr5[size] = {3, 2, 1};
+    cout << "Arr5: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr5[i] << " ";
+    }
+    cout << endl;
+
+    // int arr6[2] = {4, 5, 6, 7}; // Error*/
+
+
+
+    /*const int size = 5;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter number [" << i << "]:";
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;*/
+
+
+
+    /*const int size = 10;
+    int arr[size] = {23, 45, -58, 14, -7, -9, 32, 4, 8, -6};
+    int sum = 0;
+    int max = arr[0];
+    int min = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < 0)
+        {
+            sum += arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    int positive;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (arr[i] > 0)
+        {
+            positive = arr[i];
+        }
+    }
+    int negative;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < 0)
+        {
+            negative = arr[i];
+        }
+    }
+
+
+    cout << "Summa --> " << sum << endl;
+    cout << "Min --> " << min << endl;
+    cout << "Max --> " << max << endl;
+    cout << "Last negative --> " << negative << endl;
+    cout << "First positive --> " << positive << endl;*/
+}
