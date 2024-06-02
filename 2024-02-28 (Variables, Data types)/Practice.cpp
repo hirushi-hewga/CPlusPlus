65a6c3a66ff0e443f2f97a80b89166ff9b7b6231
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:25:01 +0300

    Practice (Variables_DataTypes)

---
+++ 2024-02-28 (Variables, Data types)/Practice.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*
+    int diagonalInInches;
+    cout << "Enter diagonal in inches: ";
+    cin >> diagonalInInches;
+    float diagonalInCentimeters;
+    diagonalInCentimeters = diagonalInInches * 2.54;
+    cout << "diagonal in centimeters --> " << diagonalInCentimeters;
+    */
+
+    /*
+    const int days = 30;
+    int feedInDay;
+    cout << "Enter feed in day (g.): ";
+    cin >> feedInDay;
+    int amountFeedIn30Days = feedInDay * days;
+    cout << "Amount feed in 30 days --> " << amountFeedIn30Days << "g.";
+    */
+
+    /*
+    cout << "=================================================================================" << endl;
+    cout << '|' << "       " << 'X' << "       ";
+    cout << '|' << "       " << 'Y' << "       ";
+    cout << '|' << "    " << "X AND Y" << "    ";
+    cout << '|' << "    " << "X OR Y" << "     ";
+    cout << '|' << "     " << "NOT X" << "     " << '|' << endl;
+    cout << "=================================================================================" << endl;
+    cout << '|' << "       " << '0' << "       ";
+    cout << '|' << "       " << '0' << "       ";
+    cout << '|' << "       " << '0' << "       ";
+    cout << '|' << "       " << '0' << "       ";
+    cout << '|' << "       " << '1' << "       " << '|' << endl;
+    cout << '|' << "       " << '0' << "       ";
+    cout << '|' << "       " << '1' << "       ";
+    cout << '|' << "       " << '0' << "       ";
+    cout << '|' << "       " << '1' << "       ";
+    cout << '|' << "       " << '1' << "       " << '|' << endl;
+    cout << '|' << "       " << '1' << "       ";
+    cout << '|' << "       " << '0' << "       ";
+    cout << '|' << "       " << '0' << "       ";
+    cout << '|' << "       " << '1' << "       ";
+    cout << '|' << "       " << '0' << "       " << '|' << endl;
+    cout << '|' << "       " << '1' << "       ";
+    cout << '|' << "       " << '1' << "       ";
+    cout << '|' << "       " << '1' << "       ";
+    cout << '|' << "       " << '1' << "       ";
+    cout << '|' << "       " << '0' << "       " << '|' << endl;
+    cout << "=================================================================================" << endl;
+    */
+}
