c689624ca48431b09c0bafc9145b989953e2a16d
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:43:03 +0300

    Practice (For_IncludeCycles)

---
+++ 2024-03-06 (For, Include cycles, Debugger)/Practice.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i <= j)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i >= j)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i >= j && i + j >= 10 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i <= j && i + j <= 10 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i >= j && i + j >= 10 - 1 || i <= j && i + j <= 10 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i >= j && i + j <= 10 - 1 || i <= j && i + j >= 10 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i >= j && i + j <= 10 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i <= j && i + j >= 10 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i + j <= 10 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*for (int i = 0; i < 10; i++){
+        for (size_t j = 0; j < 10; j++){
+            if (i + j >= 10 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }*/
+}
