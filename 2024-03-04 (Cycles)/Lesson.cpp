5d1262da6ac31309881de0b24d7ed9d3ccd0539e
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:35:43 +0300

    Lesson (Cycles)

---
+++ 2024-03-04 (Cycles)/Lesson.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //const int USA = 1;
+    //const int Ukraine = 380;
+    //const int Italy = 39;
+    //const int Polland = 48;
+    //const int Australia = 61;
+    //enum
+
+    /*enum Countries{USA = 1, Ukraine = 380, Italy = 39, Polland = 48, Australia = 61};
+    cout << USA << endl;
+
+    enum coins{penny = 1, nickel = 2, dime = 10, quarter = 25, half = 50,
+    large_dollar = 100};
+
+    enum menu{AddNewItem, ShowAll, DeleteOne, Search, Sort};
+    cout << "AddNewItem" << AddNewItem << endl;
+    cout << "ShowAll" << ShowAll << endl;
+    cout << "DeleteOne" << DeleteOne << endl;
+    cout << "Search" << Search << endl;
+    cout << "Sort" << Sort << endl;
+
+    int coin;
+    cout << "Enter the value of American coin: ";
+    cin >> coin;
+    switch (coin)
+    {
+    case penny:
+        cout << "Penny = 1 cent" << endl;
+        break;
+    case nickel:
+        cout << "Nickel = 2 cent" << endl;
+        break;
+    case dime:
+        cout << "Dime = 10 cent" << endl;
+        break;
+    case quarter:
+        cout << "Quarter = 25 cent" << endl;
+        break;
+    case half:
+        cout << "Half = 50 cent" << endl;
+        break;
+    case large_dollar:
+        cout << "Large dollar = 100 cent" << endl;
+        break;
+    default:
+        cout << "Not found!" << endl;
+        break;
+    }*/
+
+
+    /*int number = 0;
+    while (number < 10){
+        number++;
+        cout << number << ". Hello world!" << endl;
+    }*/
+
+
+    /*int answer, count = 0;
+    do{
+        cout << "Enter 1 if you want to Print Hello world!." << endl;
+        cout << "Enter 2 if you want to exit." << endl;
+        cout << "Enter your choice:";
+        cin >> answer;
+        switch (answer){
+            case 1:
+                while (count < 10){
+                    count++;
+                    cout << count << ". Hello world!" << endl;
+                }
+                count = 0;
+                break;
+            case 2:
+                cout << "Exit!" << endl;
+                break;
+            default:
+                cout << "Error!!!" << endl;
+        }
+    }
+    while (answer!=2);*/
+
+
+
+    /*//while
+    //for
+    //do while
+    int counter = 0;
+    while (counter < 7)
+        cout << "Your have seen " << ++counter << " Wonder of world" << endl;
+    cout << "Now, you can begin write your essay!!!" << endl;*/
+}
