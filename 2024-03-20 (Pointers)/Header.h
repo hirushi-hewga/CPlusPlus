6571546985a8bd8485e2b41dc920628f628d3375
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 11:22:14 +0300

    Header (Pointers)

---
+++ 2024-03-20 (Pointers)/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+int* FindMax(int* a, int* b);
+void NumberSign(int* a);
+void Calculator(int* pa, int* pb);
+void Divide(int* pa, int* pb);
+void Multiply(int* pa, int* pb);
+void Subtract(int* pa, int* pb);
+void Add(int* pa, int* pb);
+void InitArray(int* parr, int size);
+void ShowArray(int* parr, int size);
+int SummaArray(int* parr, int size);
