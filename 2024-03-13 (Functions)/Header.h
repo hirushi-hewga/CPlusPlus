bbf12dd559502c58d35e129447d4b4774f864dce
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 11:08:05 +0300

    Header (Functions)

---
+++ 2024-03-13 (Functions)/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+void square(int N, int K);
+int factorial(int number);
+bool isPrime(int number);
+int cube(int number);
+int LargerNumber(int number_1, int number_2);
+bool isPositive(int number);
+void MinMaxArray(int arr[], int size);
+void ArrayReverse(int arr[], int size);
+void CountIsPrime(int arr[], int size);
