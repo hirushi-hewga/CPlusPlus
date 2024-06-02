bf0080ee904a40995db92e375fbb17a8cf95ca20
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:29:26 +0300

    Lesson (LogicalOperators_BranchingOperators)

---
+++ 2024-03-01 (Logical operators, Branching operators)/Lesson.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //Unarny operators
+    //int number1 = -3;
+    //number1 = (-(number1));
+    //cout << number1 << endl; //3
+
+
+    //Binary operators
+    //int number2;
+    //number2 = 8 - 6;
+    //cout << number2 << endl;
+
+
+    //Ternary operators
+    //int a = 10;
+    //int b = 20;
+    //int max = (a > b) ? a : b;  
+    //cout << max << endl;               
+
+
+    //int c = 8;
+    //cout << c << endl; //print 8
+    //c++;
+    //cout << c << endl; //print 9
+
+    //int d = 8;
+    //cout << d << endl; //print 8
+    //d--;
+    //cout << d << endl; //print 7
+
+    //int e=8;
+    //cout<<++e << endl; 
+    //cout<<e++ << endl; 
+    //cout<<e << endl;   
+
+
+    //cout << !(3<2) << endl; //print 1
+    //cout << !(3>2) << endl; //print 0
+
+
+
+
+    //Logical choice - if
+    /*int number_1 = 2, number_2 = 5;
+    if (number_1 > number_2)
+        cout << number_1 << " > " << number_2 << " = true" << endl;
+    else
+        cout << number_1 << " > " << number_2 << " = false" << endl;*/
+
+
+        //int b = 1;
+        //(b==1)?cout << "b-true" << endl:cout << "b-false" << endl;
+
+
+        /*int a, b;
+        cout << "Enter first number: ";
+        cin >> a;
+        cout << "Enter second number: ";
+        cin >> b;
+        int max=(a>b)?a:b, min=(a<b)?a:b;
+        cout << "\nMin number: " << min << endl;
+        cout << "Max number: " << max << endl;*/
+
+
+        /*int choice;
+        cout << "1 - print(Hello World!)\n2 - print(Hello Heaven!)\n3 - print(Hello Hell!)\n4 - print(Hello Kitty!)" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice){
+            case 1:
+                cout << "\nHello World!" << endl;
+                break;
+            case 2:
+                cout << "\nHello Heaven!" << endl;
+                break;
+            case 3:
+                cout << "\nHello Hell!" << endl;
+                break;
+            case 4:
+                cout << "\nHello Kitty!" << endl;
+                break;
+            default:
+                cout << "\nInvalid number!" << endl;
+        }*/
+
+
+        /*int number;
+        cout << "Enter number (1-10): ";
+        cin >> number;
+        switch (number){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                cout << "\nnumber < 5" << endl;
+                break;
+            case 5:
+                cout << "\nnumber = 5" << endl;
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                cout << "\nnumber > 5" << endl;
+                break;
+            default:
+                cout << "\nInvalid number!" << endl;
+        }*/
+}
