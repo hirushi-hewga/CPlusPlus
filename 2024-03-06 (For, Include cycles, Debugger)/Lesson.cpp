385af6350cc6431243b28fde439d8f04e91c213b
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:41:34 +0300

    Lesson (For_IncludeCycles)

---
+++ 2024-03-06 (For, Include cycles, Debugger)/Lesson.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*for (int i = 0; i < 10; i++)
+    {
+        cout << i+1 << ". Hello world!" << endl;
+    }
+    int x;
+    for (x = 0; x <= 10; x++)
+    {
+        if (x%2==0)
+            cout << x << " ";
+    }
+    cout << endl;
+
+    float y = 0;
+    for (; y <= 1; y+=0.2)
+    {
+        cout << y << " ";
+    }
+    for (int i = 1; i < 5;)
+    {
+        cout << i << " ";
+        i++;
+    }
+    cout << endl;
+
+    for (int i = 0; ; i++)
+    {
+        cout << i << " ";
+        if (i == 10)break;
+    }*/
+
+
+
+    /*int magicNumber = 2;
+    cout << "================================================" << endl;
+    cout << "       My magic number between 1 and 10         " << endl;
+    cout << "================================================" << endl;
+    int user;
+
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << "Enter number --> ";
+        cin >> user;
+        if (user == magicNumber){
+            cout << "Congratulation!" << endl;
+            break;
+        }
+        else
+            cout << "That is not my number!" << endl;
+        if (i == 5)
+            cout << "Loser! Try again!"
+    }*/
+
+
+
+    /*for (int i = 0; i < 26; i++){
+        if (i%2==0)
+            continue;
+        cout << i << " ";
+    }*/
+
+
+
+    /*int sum = 0;
+    for (int i = 1; i <= 12; i++)
+    {
+        sum += i;
+    }
+    cout << "Summa = " << sum << endl;*/
+
+
+
+
+    /*for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cout << "|###|";
+        }
+        cout << endl;
+    }*/
+
+
+
+    /*for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << " * " << j << " = " << i * j << endl;
+        }
+
+    }*/
+
+
+
+    /*for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }*/
+
+
+
+    /*int str = 1, star_count, length = 20;
+    while (str <= length){
+        star_count = 1;
+        while (star_count <= length){
+            cout << "* ";
+            star_count++;
+        }
+        cout << endl;
+        str++;
+    }*/
+
+
+
+    /*for (int i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            if (i == j)
+                cout << "+ ";
+            else
+                cout << "= ";
+        }
+        cout << endl;
+    }*/
+
+
+
+    /*int N = 10;
+    for (int i = 0; i < 10; i++)
+    {
+        for (size_t j = 0; j < 10; j++)
+        {
+            if (i >= j && i + j >= N - 1)
+                cout << "|===|";
+            else
+                cout << "     ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cout << "|###|";
+        }
+        cout << endl;
+    }*/
+}
