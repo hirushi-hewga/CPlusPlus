3a0ce6fd8941b1d234c942cd13e2b9ee2bc45456
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:25:56 +0300

    HW (Variables_DataTypes)

---
+++ 2024-02-28 (Variables, Data types)/HW.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*
+    int number_1;
+    cout << "Enter first number: ";
+    cin >> number_1;
+    int number_2;
+    cout << "\nEnter second number: ";
+    cin >> number_2;
+    int number_3;
+    cout << "\nEnter third number: ";
+    cin >> number_3;
+    cout << '\n' << number_1 << " + " << number_2 << " + " << number_3 << " = " << number_1+number_2+number_3;
+    */
+
+    /*
+    float number_1;
+    cout << "Enter first number: ";
+    cin >> number_1;
+    float number_2;
+    cout << "Enter second number: ";
+    cin >> number_2;
+    cout << '(' << number_1 << " + " << number_2 << ')' << " / 2 = " << (number_1+number_2)/2;
+    */
+
+    /*
+    float kilometers;
+    cout << "Enter kilometers: ";
+    cin >> kilometers;
+    float meters = kilometers*1000;
+    cout << '\n' << kilometers << "km.\n-->\n" << meters << "m.";
+    */
+
+    /*
+    const int costKinder = 100;
+    const int costSlave = 100000;
+    const int costBook = 476;
+    const int costPepsi = 59;
+    const int countKinder = 3;
+    const int countSlave = 1;
+    const int countBook = 22;
+    const int countPepsi = 2;
+    cout << "================\nKinder: " << countKinder;
+    cout << "\nSlave: " << countSlave;
+    cout << "\nBook: " << countBook;
+    cout << "\nPepsi: " << countPepsi << "\n================\n";
+    cout << "Cost value --> " << costPepsi+costBook+costSlave+costKinder;
+    */
+
+    /*
+    int number;
+    cout << "Enter number: ";
+    cin >> number;
+    cout << number << " * " << number << " = " << number*number;
+    */
+
+    /*
+    int days;
+    cout << "Enter number of days: ";
+    cin >> days;
+    const int hoursInDay = 24;
+    const int minutesInHour = 60;
+    int minutesInDays;
+    minutesInDays = days * hoursInDay * minutesInHour;
+    cout << "Days: " << days << "\n-->\nMinutes: " << minutesInDays;
+    */
+}
