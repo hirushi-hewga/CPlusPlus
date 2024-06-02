7e1f457c94925fa5a236540165a7d30cfefb87e6
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 11:19:13 +0300

    Header (LenearSearch_Sort)

---
+++ 2024-03-18 (Lenear search, Sort)/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+int PowerOfNumber(int number, int pow);
+void InitArray(int* arr, int size);
+void ShowArray(int* parr, int size);
+void InsertSort(int arr[], int size, int value = 1);
+void InitArray2(int* parr, int size);
+void InsertSortRange(int arr[], int start, int end);
+void InitArray3(int* parr, int size);
+void ScatterElements(int arr[], int size);
+int FindNumberIndex(int arr[], int size, int number);
+void PrintStars(int count, int counter);
+void Summa(int a, int b);
+int MinSummaIndex(int arr[], int size);
