bae91c1bf586440cbbe7b3f1b616cec8590dc9b0
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 11:12:35 +0300

    Lesson (OverloadFunctions)

---
+++ 2024-03-14 (Overload funtions)/Lesson.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+//void Hello()
+//{
+//    cout << "Hello Olena" << endl;
+//}
+
+// void Hello()
+// {
+//     cout << "Hello Nikita" << endl;
+// }
+
+//inline void HelloInLine()
+//{
+//    cout << "Hello world in line" << endl;
+//}
+
+//template<typename T>
+//T max_element(T arr[], int size)
+//{
+//    T max = arr[0];
+//    for (int i = 0; i < size; i++)
+//    {
+//        if (arr[i] > max)
+//            max = arr[i];
+//    }
+//    return max;
+//}
+
+/*
+long max_element(long arr[], int size)
+{
+    long max = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+float max_element(float arr[], int size)
+{
+    float max = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+double max_element(double arr[], int size)
+{
+    double max = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+*/
+
+//double Multi(double x)
+//{
+//    return x * x * x;
+//}
+//
+//double Multi(double x, double y)
+//{
+//    return x * y * x;
+//}
+//
+//double Multi(double x, double y, double z)
+//{
+//    return x * y * z;
+//}
+
+// double Multi(double a = 1.0, double b = 1.0, double c = 1.0, double d = 1.0)
+// {
+//     return a * b + c * d;
+// }
+
+//template<typename T>
+//T Max(T a, T b)
+//{
+//    return a > b ? a : b;
+//}
+//
+//template<typename T1, typename T2>
+//T2 Max(T1 a, T2 b)
+//{
+//    return a > b ? a : b;
+//}
+
+//int main()
+//{
+    /*
+    cout << "Max = " << Max(5, 7) << endl;
+    cout << "Max = " << Max(3.14, 9.8) << endl;
+    cout << "Max = " << Max('A', 'W') << endl;
+    cout << "Max = " << Max(10, 12.36) << endl;
+
+    cout << "Multy 1 = " << Multi(5.3) << endl;
+    cout << "Multy 2 = " << Multi(3.1, 2.9) << endl;
+    cout << "Multy 3 = " << Multi(1.1, 2.2, 3.3) << endl;
+    //cout << "Multy 4 = " << Multi(1.1, 2.2, 3.3, 5.8) << endl;
+
+    int arr_int[] = { 1,8,7,9,6,12,45,36,74 };
+    cout << "Max element = " << max_element(arr_int, 9) << " in [ int array ]" << endl;
+    long arr_long[] = { 10l,20l,30l,40l,50l,60l };
+    cout << "Max element = " << max_element(arr_long, 9) << " in [ long array ]" << endl;
+    float arr_float[] = { 0.1f,0.2f,0.3f,0.4f,0.5f,0.6f,0.7f,0.8f,0.9f };
+    cout << "Max element = " << max_element(arr_float, 9) << " in [ float array ]" << endl;
+    double arr_double[] = { 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3 };
+    cout << "Max element = " << max_element(arr_double, 9) << " in [ double array ]" << endl;
+    */
+
+    // Hello();
+    // cout << "Hello world in line" << endl;//HelloInLine();
+//}
