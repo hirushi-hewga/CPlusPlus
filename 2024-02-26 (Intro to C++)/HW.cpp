ce4200207620ee77345d7278c999545631937c88
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:19:33 +0300

    HW (Intro_to_C++)

---
+++ 2024-02-26 (Intro to C++)/HW.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*
+
+    cout << 'I' << endl;
+    cout << "\tlove" << endl;
+    cout << "\t\tyou" << endl;
+    cout << "\t\t\tC++" << endl;
+    */
+
+
+
+    /*
+
+    cout << "\nName:\t\t\tBohdan" << endl;
+    cout << "Surname:\t\tKurylo" << endl;
+    cout << "Lastname:\t\tAndriyovych" << endl;
+
+    cout << "\nDate of birth:\t\t22.06.06" << endl;
+    cout << "City:\t\t\tRivne" << endl;
+    cout << "Age:\t\t\t17" << endl;
+
+    cout << "\nHobby:\t\t\tC++" << endl;
+    cout << ": : ........ . . . . . . . . ........ : :" << endl;
+    */
+}
