8d24de36ad5c453f80e60ae2818c524195cf3d8d
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:23:31 +0300

    Lesson (Variables_DataTypes)

---
+++ 2024-02-28 (Variables, Data types)/Lesson.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //      \b
+    //      \n
+    //      \t
+    //      \\
+    //      \"
+    //      \'
+    //      \b' - Backspace
+    //      \n' - New line
+    //      \t' - Horizontal tab
+    //      \\' - Backslash
+    //      \"' - Double quotation mark
+    //      \'' - Single quotation mark
+
+        /*
+        cout << "Hello world!";
+        cout << "\t\tCongratulation with good beginning!! ;)\n\n";
+        */
+
+        /*
+        short a = 5;                //2b
+        float pi = 3.14;            //4b
+        double digit = 3.3333333;   //8b
+        int b = 100;                //4b
+        b = 111;
+
+        const int c = 200;
+        //c = 222;//Error
+
+        int b1 = 5;
+        int b2 = 55;
+        int b3 = 555;
+        //int 4b = 5555;//Error
+
+        int ageofman = 100;
+        //int age of man = 115;//Error
+        int ageOfMan = 115;
+        int age_of_man = 111;
+        int _b = 23;
+        //int int = 2;//Error
+        */
+
+        /*
+        int a = 5;
+        cout << "a = " << a << endl;
+        a = 111;
+        cout << "a = " << a << endl;
+        */
+
+        /*
+        int daysIn_2000Year = 366;
+        const int hourInDay = 24;
+        int HourIn_2000_Year;
+        HourIn_2000_Year = daysIn_2000Year * hourInDay;
+        cout << "In 2000 year was " << HourIn_2000_Year << "hours\n";
+        */
+
+        /*
+        float discount = 0.05;
+        float cost = 14.25;
+        cout << "Enter cost of product: "
+        cin >> cost;
+        int count = 10;
+        cout << "Enter count of product: "
+        cin >> count;
+
+        float price;
+
+        price = cost * count - discount * cost * count;
+
+        cout << "Please, pay " << price << "grn\n";
+        */
+
+        /*
+        int number;
+        //number = 5;
+        cout << "Enter number: ";
+        cin >> number;
+        cout << number;
+        */
+}
