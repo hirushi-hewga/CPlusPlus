b425a2d98b20ec5d2fc60cd5e0f5bccc53213f19
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-02 10:45:38 +0300

    HW (For_IncludeCycles)

---
+++ 2024-03-06 (For, Include cycles, Debugger)/HW.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*int choice;
+    do{
+        int number, digitCount = 0, zeroCount = 0, sum = 0;
+        cout << "Enter number: ";
+        cin >> number;
+
+        while (number != 0) {
+            if (number%10 == 0)
+                zeroCount++;
+            sum+=number%10;
+            number = number / 10;
+            digitCount++;
+
+        }
+
+        cout << "Digit count --> " << digitCount << endl;
+        cout << "Zero count --> " << zeroCount << endl;
+        cout << "Digit suma --> " << sum << endl;
+        cout << "serednye aryfmetychne --> " << sum / digitCount << endl;
+        cout << "Do you want to exit:\n1 - yes\n2 - no\nEnter your choice: ";
+        cin >> choice;
+        choice--;
+    }while(choice!=0);*/
+
+
+
+
+    /*int size;
+    cout << "Enter number: ";
+    cin >> size;
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            if (i%2==0){
+                for (int i = 0; i < size; i++){
+                    cout << "*";
+                }
+                for (int i = 0; i < size; i++){
+                    cout << "_";
+                }
+            }
+            else{
+                for (int i = 0; i < size; i++){
+                    cout << "_";
+                }
+                for (int i = 0; i < size; i++){
+                    cout << "*";
+                }
+            }
+        }
+        cout << endl;
+    }*/
+}
