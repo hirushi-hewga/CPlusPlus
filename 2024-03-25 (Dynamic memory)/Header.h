a1e2ff24aa49d9f28ca36e8f4ec1314c5be246c0
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-03 14:08:13 +0300

    Header (DynamicMemory)

---
+++ 2024-03-25 (Dynamic memory)/Header.h
@@ -0,0 +1,7 @@
+#pragma once
+void InitArr(int* arr, int* size);
+void ShowArr(int* arr, int* size);
+int* CreateArr(int* size);
+int* AddElement(int* arr, int* size);
+int* DeleteElem(int* arr, int* size);
+int* IndexDeleteElem(int* arr, int* size);
