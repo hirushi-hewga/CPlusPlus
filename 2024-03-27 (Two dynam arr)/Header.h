3c9680e95ccf4b52245178edb405f61d442e7bfb
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-03 14:14:51 +0300

    Header (TwoDynamArr)

---
+++ 2024-03-27 (Two dynam arr)/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+void Show2DArray(int** arr, int* rows, int* cols);
+void Init2DArray(int** arr, int* rows, int* cols);
+int** Create2DArray(int* rows, int* cols);
+int** AddRow2DArray(int** arr, int* rows, int* cols);
+int** DelRow2DArray(int** arr, int* rows, int* cols);
+void InitArray(int* arr, int* size);
+int** AddRow2DArrayIndex(int** arr, int* rows, int* cols);
+int** AddCol2DArrayIndex(int** arr, int* rows, int* cols);
+int** AddCol2DArray(int** arr, int* rows, int* cols);
